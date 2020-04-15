7d9acd9b3f8785e1fee32c30a6713548ffb6e5f5
Miguel Rust <hava12soft@gmail.com>
2020-04-15 16:40:22 -0600

    Made some basic modifications.

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"> 
 
 	<?php wp_head(); ?>
 </head>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package _s
+ * @package Bricks For Books
  */
 
 get_header();
