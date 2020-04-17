9dfea39da6e7234131aa4cce09f887d3bef6c13b
Miguel Rust <hava12soft@gmail.com>
2020-04-16 21:18:12 -0600

    Rudementary blog styles

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php _s_post_thumbnail(); ?>
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<div class="title-group">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	
--- template-parts/content.php
+++ template-parts/content.php
@@ -11,25 +11,26 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+		<div clas="title-group">
+			<?php
+			if ( is_singular() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				_s_posted_on();
-				_s_posted_by();
+			if ( 'post' === get_post_type() ) :
 				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+				<div class="entry-meta">
+					<?php
+					_s_posted_on();
+					_s_posted_by();
+					?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</div>
+		<?php _s_post_thumbnail(); ?>
 	</header><!-- .entry-header -->
-
-	<?php _s_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
