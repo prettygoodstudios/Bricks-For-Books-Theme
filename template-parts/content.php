1b7d6694a394099d4026209332ed944425346a54
Miguel Rust <hava12soft@gmail.com>
2020-04-16 21:46:23 -0600

    Improved blog styling

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,13 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="post-header">
 		<div clas="title-group">
 			<?php
 			if ( is_singular() ) :
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				the_title( '<h1 class="entry-title">', '</h1>' );
 			endif;
 
 			if ( 'post' === get_post_type() ) :
@@ -29,7 +29,9 @@
 				</div><!-- .entry-meta -->
 			<?php endif; ?>
 		</div>
-		<?php _s_post_thumbnail(); ?>
+		<div class="image-holder">
+			<?php _s_post_thumbnail(); ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -49,6 +51,9 @@
 			)
 		);
 
+		if (!is_singular()) :
+			echo '<a class="read-more" href="' . esc_url( get_permalink() ) . '" rel="bookmark">Read More!</a>' ;
+		endif;
 		wp_link_pages(
 			array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
