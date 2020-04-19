1db2ec4bca2fc726c33100afd6a4503238161b8e
Jake Pope <jacob.pope@aggiemail.usu.edu>
2020-04-18 18:03:14 -0600

    added footer stylesheet and a placeholder footer section

--- footer.php
+++ footer.php
@@ -14,8 +14,15 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+		<link rel="stylesheet" href="/styles/footer.css">
+
+		<div class="footer">
+			<br>
+			<br>
+			<br>
+			<p>hello test</p>
 		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
