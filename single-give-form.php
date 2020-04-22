9a28c703b34c8f1ab9d8b334ffa97117e3e0f7c4
Miguel Rust <hava12soft@gmail.com>
2020-04-22 14:38:49 -0600

    Created some additional donation styling.

--- single-give-form.php
+++ single-give-form.php
@@ -11,14 +11,17 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-donations">
 		<?php
 		while ( have_posts() ) :
 			the_post();
 			give_get_template_part( 'single-give-form/content', 'single-give-form' );
 		endwhile; // End of the loop.
 		?>
-
+		<div class="give_forms">
+			<h1>Donate Legos</h1>
+			<?php echo FrmFormsController::get_form_shortcode( array( 'id' => 2, 'title' => false, 'description' => false ) ); ?>
+		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
