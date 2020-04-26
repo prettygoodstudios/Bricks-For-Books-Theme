b895d782a7accaaff94623048b32c84ae375a1e1
Jake Pope <jacob.pope@aggiemail.usu.edu>
2020-04-25 19:02:28 -0600

    added imbedded map didn't feel need to creat new branch

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -19,9 +19,9 @@
 
 
 
-	<div class="entry-content left ">
+	<div class="entry-content left">
 		<?php
-		echo "<div class='contact'";
+		echo "<div class='contact'>";
 
 		the_content();
 
@@ -35,6 +35,10 @@
 		);
 		?>
 	</div><!-- .entry-content -->
+
+	<div class="right">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48693.0206022887!2d-111.73370532550392!3d40.29099121832586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d7804e4b45119%3A0x642bc3cf90b21ed1!2sOrem%2C%20UT!5e0!3m2!1sen!2sus!4v1587862275612!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+	</div>
 
 	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
